9617acf6095657c0a5cfa2e42e1cceace2b8e53a
KevenRodrigues6868 <kevenlucas2015@gmail.com>
2025-05-15 18:07:13 -0300

    Utilizando o arquivo ui/app_color

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../colors.dart';
 import '../ui/app_colors.dart';
 
+
 class Header extends StatelessWidget {
   const Header({super.key});
 
@@ -21,18 +22,14 @@ class Header extends StatelessWidget {
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 22,
-              color: Color.fromARGB(255, 0, 0, 0),
+              color: AppColors.branco,
             ),
           ),
           const SizedBox(height: 4),
           const Text(
             'Quarta-Feira, 14 De Maio',
             style: TextStyle(
-<<<<<<< HEAD
-              color: Color.fromARGB(255, 0, 0, 0),
-=======
               color: AppColors.branco,
->>>>>>> a21bf1f8a37b2b04c08e7a4f7e803150df111a4b
               fontWeight: FontWeight.w500,
             ),
           ),
